e82e51d72785bf29b2ef7d13537906d7d7b70310
Alexandr Morozov <morozov1982@gmail.com>
2022-09-02 13:48:23 +0600

    add: lesson_06 db config

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -6,7 +6,8 @@ load_dotenv()
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = 'postgres://ryzwaxrcsvglft:d89851a207aa5415eafcf37572d088d55589eb40cbd84362ce7a22b53f4ec6c2@ec2-34-253-119-24.eu-west-1.compute.amazonaws.com:5432/da1ggg35aq97vr'
     SECRET_KEY = environ.get('KEY')
 
     # gmail config
